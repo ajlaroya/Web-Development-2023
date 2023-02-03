eca282e17c94d54b49b87dff7a2a8f2c6e626780
ajlaroya <arthur.j.laroya@gmail.com>
2023-02-03 21:47:56 +1100

    server ip update

--- dalle_clone/client/src/pages/Home.jsx
+++ dalle_clone/client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8081/api/v1/post", {
+        const response = await fetch("https://arthurs-dall-e.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
