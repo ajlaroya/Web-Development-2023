73cd7124ebfcf87191b5ba84ed13b98dd45f189c
ajlaroya <arthur.j.laroya@gmail.com>
2023-02-03 21:10:06 +1100

    favicon and footer

--- dalle_clone/client/src/App.jsx
+++ dalle_clone/client/src/App.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-import { logo } from './assets'
-import { Home, CreatePost } from './pages'
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
-          <img src={logo} alt="logo" className="w-28 object-contain"/>
+          <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
 
-        <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+        <Link
+          to="/create-post"
+          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        >
           Create
         </Link>
       </header>
 
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
         </Routes>
       </main>
-    </BrowserRouter>
-  )
-}
 
-export default App
+      <footer class="w-full flex justify-center items-center bg-white sm:px-8 px-4 py-6 border-t border-t-[#e6ebf4]">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{" "}
+          <a href="https://flowbite.com/" class="hover:underline">
+          • Created by Arthur Laroya™{" "}
+          </a>
+          • All Rights Reserved.
+        </span>
+      </footer>
+    </BrowserRouter>
+  );
+};
+
+export default App;
