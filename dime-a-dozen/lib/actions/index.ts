b078046078bdb5751f2db8785bd767ec004642da
ajlaroya <arthur.j.laroya@gmail.com>
2023-12-10 14:51:05 +1100

    product object

--- dime-a-dozen/lib/actions/index.ts
+++ dime-a-dozen/lib/actions/index.ts
@@ -6,7 +6,11 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
 
     try {
+        // timestamp 1:48:39 DB
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+        if(!scrapedProduct) return;
+        // 
     } catch (error:any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
     }
