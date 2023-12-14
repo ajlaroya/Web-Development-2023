9d8c537892e13579003189eb84127cd6237847d6
ajlaroya <arthur.j.laroya@gmail.com>
2023-12-14 15:34:08 +1100

    mongodb connect, models

--- dime-a-dozen/lib/actions/index.ts
+++ dime-a-dozen/lib/actions/index.ts
@@ -1,16 +1,21 @@
 "use server"
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
 
     try {
-        // timestamp 1:48:39 DB
+        connectToDB();
+
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
         if(!scrapedProduct) return;
-        // 
+        
+        let product = scrapedProduct;
+
+
     } catch (error:any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
     }
